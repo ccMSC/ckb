17c068ea33df8677a8b9e1818318278d296056e9
Karl Fleischmann <fleischmann.karl@gmail.com>
2016-06-14 14:04:31 +0200

    Tidy cli
    * Inline `resolveCommand` querries to be more concise.
    * Let `CommandLine` methods return `CommandLine` status codes.
    * Remove excessive calls to `.toLocal8Bit().data()`.

--- src/ckb/cli.cpp
+++ src/ckb/cli.cpp
@@ -6,61 +6,29 @@
  * resolveCommand - Resolve the given String to a Command identifier.
  */
 int Command::resolveCommand(QString cmd) {
-    if (cmd.compare("global") == 0) {
-        return CommandGlobal;
-    }
-    else if (cmd.compare("device") == 0) {
-        return CommandDevice;
-    }
-    else if (cmd.compare("profile") == 0) {
-        return CommandProfile;
-    }
-    else if (cmd.compare("help") == 0) {
-        return CommandHelp;
-    }
-    else if (cmd.compare("info") == 0) {
-        return CommandInfo;
-    }
-    else if (cmd.compare("layout") == 0) {
-        return CommandLayout;
-    }
-    else if (cmd.compare("modifier") == 0) {
-        return CommandModifier;
-    }
-    else if (cmd.compare("framerate") == 0) {
-        return CommandFramerate;
-    }
-    else if (cmd.compare("animation-dir") == 0) {
-        return CommandAnimationDir;
-    }
-    else if (cmd.compare("brightness-per-mode") == 0) {
-        return CommandBrightnessPerMode;
-    }
-    else if (cmd.compare("spatial-dithering") == 0) {
-        return CommandSpatialDithering;
-    }
-    else if (cmd.compare("firmware-autocheck") == 0) {
-        return CommandFirmwareAutocheck;
-    }
-    else if (cmd.compare("tray-icon") == 0) {
-        return CommandTrayIcon;
-    }
-    else if (cmd.compare("mouse-acceleration") == 0) {
-        return CommandMouseAcceleration;
-    }
-    else if (cmd.compare("scroll-acceleration") == 0) {
-        return CommandScrollAcceleration;
-    }
-    else if (cmd.compare("scroll-acceleration-speed") == 0) {
-        return CommandScrollAccelerationSpeed;
-    }
+    if (cmd.compare("global") == 0) return Command::CommandGlobal;
+    else if (cmd.compare("device") == 0) return Command::CommandDevice;
+    else if (cmd.compare("profile") == 0) return Command::CommandProfile;
+    else if (cmd.compare("help") == 0) return Command::CommandHelp;
+    else if (cmd.compare("info") == 0) return Command::CommandInfo;
+    else if (cmd.compare("layout") == 0) return Command::CommandLayout;
+    else if (cmd.compare("modifier") == 0) return Command::CommandModifier;
+    else if (cmd.compare("framerate") == 0) return Command::CommandFramerate;
+    else if (cmd.compare("animation-dir") == 0) return Command::CommandAnimationDir;
+    else if (cmd.compare("brightness-per-mode") == 0) return Command::CommandBrightnessPerMode;
+    else if (cmd.compare("spatial-dithering") == 0) return Command::CommandSpatialDithering;
+    else if (cmd.compare("firmware-autocheck") == 0) return Command::CommandFirmwareAutocheck;
+    else if (cmd.compare("tray-icon") == 0) return Command::CommandTrayIcon;
+    else if (cmd.compare("mouse-acceleration") == 0) return Command::CommandMouseAcceleration;
+    else if (cmd.compare("scroll-acceleration") == 0) return Command::CommandScrollAcceleration;
+    else if (cmd.compare("scroll-acceleration-speed") == 0) return Command::CommandScrollAccelerationSpeed;
 
-    return CommandUnknown;
+    return Command::CommandUnknown;
 }
 
 int CommandLine::runGlobal() {
     if (cmdOffset >= commands.length()) return Command::CommandUnknown;
-    switch (Command::resolveCommand(commands[cmdOffset++].toLocal8Bit().data())) {
+    switch (Command::resolveCommand(commands[cmdOffset++])) {
     case Command::CommandInfo:
         std::cout << "  Print Global Info" << std::endl;
         break;
@@ -68,22 +36,22 @@ int CommandLine::runGlobal() {
         if (cmdOffset >= commands.length()) return Command::CommandUnknown;
 
         // further specify the layout command
-        QString task = commands[cmdOffset++].toLocal8Bit().data();
+        QString task = commands[cmdOffset++];
         if (task.compare("list") == 0) {
             std::cout << "  Print Global Layouts" << std::endl;
         }
         else if (task.compare("set") == 0) {
             if (cmdOffset >= commands.length()) return Command::CommandUnknown;
-            QString layout = commands[cmdOffset++].toLocal8Bit().data();
+            QString layout = commands[cmdOffset++];
             std::cout << "  Set Global Layout to " << layout.toLocal8Bit().data() << std::endl;
         }
         else {
-            return Command::CommandUnknown;
+            return CommandLineUnknown;
         }
         break;
     }
 
-    return Command::CommandOK;
+    return CommandLineOK;
 }
 
 /**
@@ -94,11 +62,10 @@ int CommandLine::run() {
 
     // parse commands and execute requested operation
     if (cmdOffset >= commands.length()) return CommandLineUnknown;
-    switch (Command::resolveCommand(commands[cmdOffset++].toLocal8Bit().data())) {
+    switch (Command::resolveCommand(commands[cmdOffset++])) {
     case Command::CommandGlobal:
         std::cout << "Global: " << std::endl;
         return runGlobal();
-        break;
     case Command::CommandDevice:
         std::cout << "Device:" << std::endl;
         break;
@@ -127,7 +94,7 @@ int CommandLine::execute(QStringList args) {
     int index = 0;
     QString item;
     for (constIterator = args.constBegin(); constIterator != args.constEnd(); ++constIterator) {
-        item = (*constIterator).toLocal8Bit().data();
+        item = (*constIterator);
         if (index == 0) {
             // first arg is the base
             cli.base = item;
